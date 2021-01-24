b5e4ce77cc80bb125d89423ff0045119a1176018
Connieh1 <connie2630a@gmail.com>
2021-01-24 12:12:19 -0800

    Import 'get_close_matches', build conditional statement for close match cases

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -7,8 +8,17 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y or N: " %
+                   get_close_matches(word, data.keys())[0])
+        if yn in ["Y", 'y']:
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yn in ["N", 'n']:
+            return "We didn't understand your entry."
+        else:
+            return "Please try again."
     else:
-        return "Word is not present in database."
+        return "Word is not present in database. Please check again."
 
 
 word = input("Enter word: ")
